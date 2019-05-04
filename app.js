3edd459fa811c982a108870ec4cad40c0bfe3f93
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2019-05-04 22:54:06 +0200

    pink theme, local storage for timer data

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const Toolbar = {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24" height="24" viewBox="0 0 24 24"
-            fill="none" stroke="#db2f27" stroke-width="2" stroke-linecap="square"
+            fill="none" stroke="#e91e63" stroke-width="2" stroke-linecap="square"
             stroke-linejoin="arcs"
           >
             <line x1="18" y1="20" x2="18" y2="10"></line>
@@ -47,7 +47,7 @@ const Toolbar = {
         <nav-link href="#settings">
           <svg
             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-            fill="none" stroke="#db2f27" stroke-width="2" stroke-linecap="square"
+            fill="none" stroke="#e91e63" stroke-width="2" stroke-linecap="square"
             stroke-linejoin="arcs"
           >
             <circle cx="12" cy="12" r="3"></circle>
@@ -79,20 +79,23 @@ const DayStats = {
     items: function() {
       return Array(+this.total)
         .fill()
-        .map((item, i) => ({finished: i < +this.finished, half: i === +this.finished}));
+        .map((item, i) => ({
+          finished: i < +this.finished,
+          half: i === +this.finished,
+        }));
     },
   },
 };
 
 const Countdown = {
-  props: ['total', 'current', 'stopped', 'paused'],
+  props: ['total', 'current', 'running'],
   template: `
     <div class="timer-container">
       <div class="timer">
         <svg class="arc" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
           <defs>
             <pattern id="diagonal-hatch" patternUnits="userSpaceOnUse" width="4" height="4">
-              <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" style="stroke:#db2f27; stroke-width:0.5" />
+              <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" style="stroke:#e91e63; stroke-width:0.5" />
             </pattern>
           </defs>
           <path class="arc-path" style="fill: url(#diagonal-hatch)" :d="arc"></path>
@@ -102,13 +105,13 @@ const Countdown = {
           <div class="countdown">
             {{minutes}}
           </div>
-          <div v-if="stopped" class="buttons">
+          <div v-if="!running && current === 0" class="buttons">
             <div class="btn" v-on:click="$emit('work')">WORK</div>
             <div class="btn" v-on:click="$emit('break')">BREAK</div>
           </div>
           <div v-else="" class="buttons">
             <div class="btn" v-on:click="$emit('stop')">STOP</div>
-            <div v-if="paused" class="btn" v-on:click="$emit('resume')">RESUME</div>
+            <div v-if="!running && current > 0" class="btn" v-on:click="$emit('resume')">RESUME</div>
             <div v-else="" class="btn" v-on:click="$emit('pause')">PAUSE</div>
           </div>
         </div>
@@ -118,17 +121,19 @@ const Countdown = {
   computed: {
     minutes: function() {
       const minutes = (this.current / 60) | 0;
-      const seconds = (this.current % 60);
-      return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      const seconds = this.current % 60;
+      return `${minutes < 10 ? '0' : ''}${minutes}:${
+        seconds < 10 ? '0' : ''
+      }${seconds}`;
     },
     arc: function() {
-      let degrees = (this.current) * 359.99 / this.total;
+      let degrees = (this.current * 359.99) / this.total;
       const round = 360 / 32;
       //degrees = Math.round(degrees / round) * round;
       const polarToCartesian = (cx, cy, r, angle) => {
-        let rad = (angle - 90) * Math.PI / 180;
-        return { x: cx + (r * Math.cos(rad)), y: cy + (r * Math.sin(rad)) };
-      }
+        let rad = ((angle - 90) * Math.PI) / 180;
+        return {x: cx + r * Math.cos(rad), y: cy + r * Math.sin(rad)};
+      };
       const start = polarToCartesian(100, 100, 100, degrees);
       const end = polarToCartesian(100, 100, 100, 0);
       const arcSweep = degrees <= 180 ? '0' : '1';
@@ -143,15 +148,14 @@ const Countdown = {
   },
 };
 
-const timer = {
-    stopped: false,
-    paused: false,
-    total: 1500,
-    current: 668,
-    workDuration: 1500,
-    breakDuration: 300,
+let timer = {
+  tid: undefined,
+  total: 1500,
+  current: 668,
+  workDuration: 1500,
+  breakDuration: 300,
 };
-const startTimer = (interval) => {
+const startTimer = interval => {
   if (timer.tid) {
     stopTimer();
   }
@@ -164,39 +168,64 @@ const startTimer = (interval) => {
     if (timer.current === 0) {
       stopTimer(true);
     }
+    localStorage.setItem('timer-v1', JSON.stringify(timer));
   }, 1000);
+  localStorage.setItem('timer-v1', JSON.stringify(timer));
 };
-const stopTimer = (reset) => {
+const stopTimer = reset => {
   clearInterval(timer.tid);
   timer.tid = undefined;
   if (reset) {
     timer.current = 0;
   }
+  localStorage.setItem('timer-v1', JSON.stringify(timer));
+};
+
+timer = JSON.parse(localStorage.getItem('timer-v1') || JSON.stringify(timer));
+if (timer.tid) {
+  startTimer();
 }
 
 const RouteMain = {
-  data: function() { return timer; },
+  data: function() {
+    return timer;
+  },
   template: `
     <div>
       <toolbar />
-      <countdown :total="total" :current="current" :paused="paused" :stopped="stopped"
+      <countdown :total="total" :current="current" :running="!!tid"
       v-on:pause="pause" v-on:resume="resume" v-on:stop="stop"
       v-on:work="startWork" v-on:break="startBreak" />
       <day-stats total="7" finished="3" />
     </div>
   `,
   components: {
-    'toolbar': Toolbar,
-    'countdown': Countdown,
+    toolbar: Toolbar,
+    countdown: Countdown,
     'day-stats': DayStats,
   },
   methods: {
-    pause: function() { timer.paused = true; stopTimer(); },
-    resume: function() { timer.paused = false; startTimer(); },
-    stop: function() { timer.stopped = true; stopTimer(true); },
-    startWork: function() { timer.stopped = false; startTimer(timer.workDuration); },
-    startBreak: function() { timer.stopped = false; startTimer(timer.breakDuration); },
-  }
+    pause: function() {
+      timer.paused = true;
+      stopTimer();
+    },
+    resume: function() {
+      timer.paused = false;
+      startTimer();
+    },
+    stop: function() {
+      timer.stopped = true;
+      stopTimer(true);
+    },
+    startWork: function() {
+      timer.stopped = false;
+      startTimer(timer.workDuration);
+    },
+    startBreak: function() {
+      timer.stopped = false;
+      startTimer(timer.breakDuration);
+    },
+  },
 };
 
 //
